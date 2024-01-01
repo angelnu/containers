e11063783a64775690a1ac36e1770786eb3534cb
angelnu <git@angelnu.com>
2024-01-01 13:10:46 +0000

    parse version

--- apps/kicad-novnc/ci/latest.sh
+++ apps/kicad-novnc/ci/latest.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-version="7.0.0"
+# renovate: datasource=docker depName=ubuntu versioning=docker
+ubuntu_version="24.04"
+
+version="$(curl https://ppa.launchpadcontent.net/kicad/kicad-7.0-releases/ubuntu/pool/main/k/kicad/|sed -nr 's/.*kicad_(.*)~.*'${ubuntu_version}'.*/\1/p'|tail -n1)"
 printf "%s" "${version}"
