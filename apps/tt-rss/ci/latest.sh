67634d0636f066191f2fe76837f39738909f8bab
Angel Nunez Mencias <github@angelnu.com>
2025-12-22 12:41:29 +0100

    Update git clone URL to use GitHub repository

--- apps/tt-rss/ci/latest.sh
+++ apps/tt-rss/ci/latest.sh
@@ -8,7 +8,7 @@ if [ -d "$CHECKOUT_DIR" ]; then
   cd "$CHECKOUT_DIR"
   git pull > /dev/null
 else
-  git clone https://git.tt-rss.org/fox/tt-rss.git "$CHECKOUT_DIR" > /dev/null 2>&1
+  git clone https://github.com/tt-rss/tt-rss.git "$CHECKOUT_DIR" > /dev/null 2>&1
   cd "$CHECKOUT_DIR"
 fi
 
